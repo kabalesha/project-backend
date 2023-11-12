1684120ed87936d5ea1b1575b422ac7d838099b9
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-11-12 21:58:00 +0100

    add route api-docs

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ import cors from "cors";
 import errorGlobal from "./middlewares/errorGlobal.js";
 import errorNotFound from "./middlewares/errorNotFound.js";
 import "dotenv/config";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "./swagger.json" assert { type: 'json' };
 
 // import authRouter from "./routes/api/auth-router.js";
 // import moviesRouter from "./routes/api/movies-router.js";
@@ -15,6 +17,9 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+//Documentation swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use("/api/auth", authRouter);
 // app.use("/api/movies", moviesRouter);
