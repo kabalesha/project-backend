d427636689724947082e619cbe0346c1d3b06087
Evgeniy-D-2022 <116091668+Evgeniy-D-2022@users.noreply.github.com>
2023-11-14 00:52:44 +0100

    update function addAvatar

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -12,11 +12,19 @@ import { nanoid } from "nanoid";
 const {JWT_SECRET, BASE_URL} = process.env;
 const avatarsPath = path.resolve("public", "avatars");
 
- const addAvatar = async(req, res) => {
-    res.json({ 
-        file: req.file 
-    })
-}
+ 
+const addAvatar = async (req, res) => {
+    try {
+      const { _id } = req.user;
+      const avatarURL = req.file.path;
+  
+      await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
+      res.status(200).json({ avatarURL });
+      
+    } catch (error) {
+      res.status(500).json({ message: 'Server error' });
+    }
+  };
     
  const getCurrent = async(req, res) => {
     const {name, email, avatarURL} = req.user;
