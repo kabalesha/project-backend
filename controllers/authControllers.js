84fd724908f957ddbd9d3e7f2dfb2b2781582040
Sergey Kuran <kuran.serg@gmail.com>
2023-11-15 19:41:28 +0200

    Small fix

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,18 +1,20 @@
 import ControllerWrapper from "../utils/ControllerWrapper.js";
 import authService from "../services/authServices.js";
 
-const singUp = async (req, res) => {
-  const user = await authService.singUp(req.body);
+const signUp = async (req, res) => {
+  const user = await authService.signUp(req.body);
 
   res.status(201).json({
+    user,
     user: {
+      id: user._id,
       email: user.email,
     },
   });
 };
 
-const singIn = async (req, res) => {
-  const { token, user } = await authService.singIn(req.body);
+const signIn = async (req, res) => {
+  const { token, user } = await authService.signIn(req.body);
 
   res.json({
     token,
@@ -22,9 +24,17 @@ const singIn = async (req, res) => {
   });
 };
 
+// const logout = async (req, res) => {
+//   console.log("req.body :>> ", req.user);
+//   const user = await authService.logout(req.body);
+//   console.log("user2222 :>> ", user);
+//   res.status(204).json({ message: "Logout success" });
+// };
+
 const authController = {
-  singUp: ControllerWrapper(singUp),
-  singIn: ControllerWrapper(singIn),
+  signUp: ControllerWrapper(signUp),
+  signIn: ControllerWrapper(signIn),
+  //   logout: ControllerWrapper(logout),
 };
 
 export default authController;
