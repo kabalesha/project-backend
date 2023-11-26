b5641636deeda171a2c9e753402be91881b374e0
Sergey Kuran <kuran.serg@gmail.com>
2023-11-27 00:43:58 +0200

    Edit

--- helpers/index.js
+++ helpers/index.js
@@ -3,3 +3,4 @@ export { default as runValidatorsAtUpdate } from "./runValidatorsAtUpdate.js";
 export { default as calculateDailyFulfillment } from "./calculator.js";
 export { default as regroupedDataByDays } from "./regroupedDataByDays.js";
 export { default as formatDate } from "./formatDate.js";
+export { default as formatDateBD } from "./formatDateBD.js";
