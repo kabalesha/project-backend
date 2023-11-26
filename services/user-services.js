1682b8b976b897966736be1b2d00a28d74c085b1
Sergey Kuran <kuran.serg@gmail.com>
2023-11-27 00:14:34 +0200

    Update

--- services/user-services.js
+++ services/user-services.js
@@ -1,13 +1,17 @@
 import User from "../models/User.js";
 import HttpError from "../middlewares/httpError.js";
 
-const updateUserDataService = async (_id, body) => {
-  const user = await User.UserNew.findOneAndUpdate({ _id }, body, {
-    new: true,
-  });
+const updateUserDataService = async (_id, avatar) => {
+  const user = await User.UserNew.findOneAndUpdate(
+    { _id },
+    { avatarURL: avatar },
+    {
+      new: true,
+    }
+  );
 
   const { avatarURL } = user;
-
+  console.log("avatarURL :>> ", avatarURL);
   if (!user) throw HttpError(404, "User not found");
 
   return { avatarURL };
